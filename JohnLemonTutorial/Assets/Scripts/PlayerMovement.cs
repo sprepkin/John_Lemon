6d9f45657c7a9bbb2a73fdb009d97157f71b0437
srepkin <srepkin@iu.edu>
2021-03-09 16:28:24 -0600

    Created movement vectors

--- JohnLemonTutorial/Assets/Scripts/PlayerMovement.cs
+++ JohnLemonTutorial/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Vector3 m_Movement; //create movement vector
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //create variables for horizontal and vertical movements
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        //set movement and normalize it (to a unit of 1)
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
     }
 }
