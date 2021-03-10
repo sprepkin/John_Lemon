11074706007d17d15c56cb579f9518f042f1dfea
srepkin <srepkin@iu.edu>
2021-03-09 19:02:33 -0600

    Added update method to GameEnding

--- JohnLemonTutorial/Assets/Scripts/GameEnding.cs
+++ JohnLemonTutorial/Assets/Scripts/GameEnding.cs
@@ -7,11 +7,23 @@ public class GameEnding : MonoBehaviour
     public float fadeDuration = 1f; //duration of fade variable
     public GameObject player; //Reference John Lemon (player) character
 
+    bool m_IsPlayerAtExit; //variable for if player is at the exit
+
     //executes on trigger of collider
     void OnTriggerEnter(Collider other)
     {
         //make sure only John Lemon can trigger
         if (other.gameObject == player)
+        {
+            //set player is at exit to true
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    void Update()
+    {
+        //check if exit is set
+        if (m_IsPlayerAtExit)
         {
 
         }
