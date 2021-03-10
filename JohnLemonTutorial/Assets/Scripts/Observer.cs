c2c4c27189c8b0aecbebed2eeeb99e319ca4db09
srepkin <srepkin@iu.edu>
2021-03-09 19:53:57 -0600

    Added class to detect player in Observer

---
+++ JohnLemonTutorial/Assets/Scripts/Observer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    public Transform player; //variable to detect player
+
+    bool m_IsPlayerInRange; //variable if player in contact with line of sight
+
+    //when collides with trigger
+    void OnTriggerEnter(Collider other)
+    {   
+        //if trigger is player
+        if (other.transform == player)
+        {
+            //player is within range
+            m_IsPlayerInRange = true;
+        }
+    }
+
+    //check if something is in way of player and trigger
+    void OnTriggerExit(Collider other)
+    {
+        //if trigger is player
+        if (other.transform == player)
+        {
+            //player isn't in range
+            m_IsPlayerInRange = false;
+        }
+    }
+}
