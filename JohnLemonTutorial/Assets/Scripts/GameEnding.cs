6da8174e196621927e1654c0e5a8836b7c44bf90
srepkin <srepkin@iu.edu>
2021-03-09 20:27:54 -0600

    Added restart level to GameEnding

--- JohnLemonTutorial/Assets/Scripts/GameEnding.cs
+++ JohnLemonTutorial/Assets/Scripts/GameEnding.cs
@@ -25,23 +25,30 @@ public class GameEnding : MonoBehaviour
         }
     }
 
+    //executes caught player
+    public void CaughtPlayer()
+    {
+        //player is set to caught
+        m_IsPlayerCaught = true;
+    }
+
     void Update()
     {
         //check if player is at exit
         if (m_IsPlayerAtExit)
         {
             //trigger exit end of level
-            EndLevel(exitBackgroundImageCanvasGroup);
+            EndLevel(exitBackgroundImageCanvasGroup, false);
         //check if player was caught
         else if (m_IsPlayerCaught)
         {
             //trigger caught end of level
-            EndLevel(caughtBackgroundImageCanvasGroup);
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
         }
     }
 
     //executes on end of level
-    void EndLevel(CanvasGroup imageCanvasGroup)
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         //timer increases by change in time
         m_Timer += Time.deltaTime; 
@@ -52,8 +59,17 @@ public class GameEnding : MonoBehaviour
         //if the timer is greater than the fade duration and image display duration
         if (m_Timer > fadeDuration + displayImageDuration)
         {
-            //quit the game
-            Application.Quit();
+            //if restart is called
+            if (doRestart)
+            {
+                //load scene at start
+                SceneManager.LoadScene (0);
+            }
+            else
+            {
+                //Quit the game
+                Application.Quit ();
+            }
         }
     }
 }
