669b614c11928053251f13c93a2658776caf95fd
srepkin <srepkin@iu.edu>
2021-03-09 20:03:51 -0600

    Check enemy line of sight in Observer

--- JohnLemonTutorial/Assets/Scripts/Observer.cs
+++ JohnLemonTutorial/Assets/Scripts/Observer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Observer : MonoBehaviour
 {
     public Transform player; //variable to detect player
+    public GameEnding gameEnding; //reference gameEnding class
 
     bool m_IsPlayerInRange; //variable if player in contact with line of sight
 
@@ -27,6 +28,30 @@ public class Observer : MonoBehaviour
         {
             //player isn't in range
             m_IsPlayerInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        //if player is in range
+        if (m_IsPlayerInRange)
+        {
+            //variable direction is looking toward John Lemon's center
+            Vector3 direction = player.position - transform.position + Vector3.up;
+            //creates ray in direction of direction (above)
+            Ray ray = new Ray(transform.position, direction);
+            //define raycastHit variable
+            RaycastHit raycastHit;
+
+            //check if anything has been hit in the ray and in range
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                //check if what was hit by the ray is the player
+                if (raycastHit.collider.transform == player)
+                {
+
+                }
+            }
         }
     }
 }
