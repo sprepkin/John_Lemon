3eeb6cae62cd72ad74c8ed49464fa1bb0051aa96
srepkin <srepkin@iu.edu>
2021-03-09 19:10:54 -0600

    wrote if-statement code block in GameEnding

--- JohnLemonTutorial/Assets/Scripts/GameEnding.cs
+++ JohnLemonTutorial/Assets/Scripts/GameEnding.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f; //duration of fade variable
+    public float displayImageDuration = 1f; //duration of image display
     public GameObject player; //Reference John Lemon (player) character
+    public CanvasGroup exitBackgroundImageCanvasGroup; //Canvas group for the end game image
 
     bool m_IsPlayerAtExit; //variable for if player is at the exit
+    float m_Timer; //variable for timer
 
     //executes on trigger of collider
     void OnTriggerEnter(Collider other)
@@ -22,10 +25,28 @@ public class GameEnding : MonoBehaviour
 
     void Update()
     {
-        //check if exit is set
+        //check if player is at exit
         if (m_IsPlayerAtExit)
         {
+            //trigger the end of the level if player is at exit
+            EndLevel();
+        }
+    }
 
+    //executes on end of level
+    void EndLevel()
+    {
+        //timer increases by change in time
+        m_Timer += Time.deltaTime; 
+
+        //change exit image's alpha by time over fade duration
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        //if the timer is greater than the fade duration and image display duration
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            //quit the game
+            Application.Quit();
         }
     }
 }
